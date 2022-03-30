06c04e9c1805e7e717563e92192c569ee6573c27
Saif Khan <saifkhichi96@gmail.com>
2022-03-30 13:10:00 +0200

    Caching updates in the DataSource/Repository classes.

--- app/src/main/java/dev/aspirasoft/vread/core/data/DataSource.kt
+++ app/src/main/java/dev/aspirasoft/vread/core/data/DataSource.kt
@@ -7,7 +7,13 @@ abstract class DataSource<T : Any> constructor(var db: RemoteDatabase, var type:
 
     abstract val root: String
 
-    suspend fun add(item: T) = add(db.createEmptyChild(root), item)
+    fun create(): String {
+        return db.createEmptyChild(root)
+    }
+
+    suspend fun add(item: T) {
+        add(create(), item)
+    }
 
     suspend fun add(itemId: String, item: T) {
         db.createChild(root, itemId, item)
@@ -20,6 +26,13 @@ abstract class DataSource<T : Any> constructor(var db: RemoteDatabase, var type:
 
     suspend fun getAll(): List<T> {
         return db.list(root, type, null, null)
+    }
+
+    suspend fun update(itemId: String, item: T): Boolean {
+        if (itemId.isBlank()) return false
+
+        db.update("$root/$itemId", item)
+        return true
     }
 
     suspend fun delete(itemId: String) = db.remove("$root/$itemId")
--- app/src/main/java/dev/aspirasoft/vread/core/data/Repository.kt
+++ app/src/main/java/dev/aspirasoft/vread/core/data/Repository.kt
@@ -8,32 +8,44 @@ abstract class Repository<T : Any> constructor(
     private var cache: LocalDatabase,
 ) {
 
-    var data: HashMap<String, T> = getCached(dataSource.type)
+    var data: ArrayList<T> = getCached()
+        private set
+
+    fun create(): String {
+        return dataSource.create()
+    }
 
     suspend fun add(item: T) {
         dataSource.add(item)
+        cache(item)
     }
 
     suspend fun add(itemId: String, item: T) {
         dataSource.add(itemId, item)
-        cache(itemId, item)
+        cache(item)
     }
 
     suspend fun get(itemId: String): T? {
         val remoteItem = dataSource.get(itemId)
-        remoteItem?.let { cache(itemId, it) }
+        remoteItem?.let { cache(it) }
         return remoteItem
     }
 
     open suspend fun getAll(): List<T> {
         val remoteData = dataSource.getAll()
-        remoteData.forEach { item -> cache(item.toString(), item) }
+        cache(remoteData)
         return remoteData
     }
 
+    suspend fun update(itemId: String, item: T): Boolean {
+        val updated = dataSource.update(itemId, item)
+        if (updated) cache(item)
+        return updated
+    }
+
     suspend fun delete(itemId: String) {
+        get(itemId)?.let { deleteCached(it) }
         dataSource.delete(itemId)
-        deleteCached(itemId)
     }
 
     suspend fun clear() {
@@ -41,43 +53,44 @@ abstract class Repository<T : Any> constructor(
         clearCache()
     }
 
-    private fun cache(itemId: String, item: T) {
-        val cachedItems = cache.getOrDefault<ArrayList<String>>(dataSource.root, arrayListOf())
-        cachedItems.add(itemId)
-        cache.update(dataSource.root, cachedItems)
-
-        cache.create("${dataSource.root}_${itemId}", item)
-        data[itemId] = item
-    }
-
-    private fun getCached(itemId: String, type: Class<T>): T? {
-        return kotlin.runCatching { cache.getOrThrow("${dataSource.root}_${itemId}", type) }.getOrNull()
-    }
-
-    private fun getCached(type: Class<T>): HashMap<String, T> {
-        val cachedItems = HashMap<String, T>()
-        val cachedItemIds = cache.getOrDefault<ArrayList<String>>(dataSource.root, arrayListOf())
-        cachedItemIds.forEach { itemId ->
-            getCached(itemId, type)?.let { item: T -> cachedItems[itemId] = item }
+    private fun cache(item: T) {
+        val cachedItem = try {  // Try to get the cached item
+            val itemId = item::class.java.getDeclaredField("id").apply { isAccessible = true }.get(item)
+            data.find { it::class.java.getDeclaredField("id").apply { isAccessible = true }.get(it) == itemId }
+        } catch (ex: NoSuchFieldException) { // Item does not have an "id" field
+            data.find { it == item }  // Use object equality
+        } catch (ex: IllegalAccessException) {  // The "id" field is not accessible for some reason
+            data.find { it == item }  // Use object equality
         }
-        return cachedItems
+
+        when (cachedItem) {
+            null -> data.add(item)  // Add new item
+            else -> data[data.indexOf(cachedItem)] = item  // Replace existing item
+        }
+
+        cache.update(dataSource.root, data)
     }
 
-    private fun deleteCached(itemId: String) {
-        val cachedItems = cache.getOrDefault<ArrayList<String>>(dataSource.root, arrayListOf())
-        cachedItems.remove(itemId)
-        cache.update(dataSource.root, cachedItems)
+    private fun cache(data: List<T>) {
+        this.data.clear()
+        this.data.addAll(data)
+        cache.update(dataSource.root, data)
+    }
 
-        cache.remove("${dataSource.root}_${itemId}")
-        data.remove(itemId)
+    private fun getCached(): ArrayList<T> {
+        return cache.getOrDefault(dataSource.root, arrayListOf())
+    }
+
+    private fun deleteCached(item: T) {
+        if (data.contains(item)) {
+            data.remove(item)
+        }
+
+        cache.update(dataSource.root, data)
     }
 
     private fun clearCache() {
-        val cachedItems = cache.getOrDefault<ArrayList<String>>(dataSource.root, arrayListOf())
-        cachedItems.forEach { itemId -> cache.remove("${dataSource.root}_${itemId}") }
         cache.remove(dataSource.root)
-
-        data.clear()
     }
 
 }
